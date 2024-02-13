57244c56ba707e769e86c838c66dd11ef28b0fce
Ivan Diaz <ivdiazsa@microsoft.com>
2024-02-12 16:13:04 -0800

    Added a new template to generate code blocks in org mode.

--- emacs-files/emacs.d/org-mode-config/my-org-mode-config.el
+++ emacs-files/emacs.d/org-mode-config/my-org-mode-config.el
@@ -17,6 +17,18 @@
 (unless (boundp 'org-capture-templates)
   (setq-default org-capture-templates '()))
 
+;; Simple template for writing code snippets in org mode.
+
+(define-skeleton org-code-block-template
+  "Generate a basic template for an org-mode babel code block. This includes the
+name, header, and source segments."
+  ""
+  > "#+NAME: " (skeleton-read "Code Block's Name (if any): ") \n
+  > "#+HEADER: :var" \n
+  > "#+BEGIN_SRC " (skeleton-read "Code Block's language: ") " :results output" \n
+  > \n
+  > "#+END_SRC")
+
 ;; Load each personalized org-mode configuration files.
 
 (load-file "~/.emacs.d/org-mode-config/japanese-dictionary-orgmode.el")
