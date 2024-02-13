209dba73411b01ebb542ef5fdeeffa4bc36c13ea
Ivan Diaz <ivdiazsa@microsoft.com>
2024-02-12 16:13:49 -0800

    Added a new terminal-themes folder, and initialized it with a small file with the GNOME dark terminal color scheme palette.

---
+++ emacs-files/emacs.d/terminal-themes/gnome-dark.el
@@ -0,0 +1,14 @@
+;; Terminal colors from the GNOME Dark Terminal color scheme palette.
+
+(custom-set-faces
+ '(term-color-black   ((t (:foreground "#171421"))))
+ '(term-color-blue    ((t (:foreground "#2A7BDE"))))
+ '(term-color-cyan    ((t (:foreground "#33C7DE"))))
+ '(term-color-green   ((t (:foreground "#33DA7A"))))
+ '(term-color-magenta ((t (:foreground "#C061CB"))))
+ '(term-color-red     ((t (:foreground "#F66151"))))
+ '(term-color-white   ((t (:foreground "#D0CFCC"))))
+ '(term-color-yellow  ((t (:foreground "#E9AD0C"))))
+ '(term-default-bg-color ((t (:inherit term-color-black))))
+ '(term-default-fg-color ((t (:inherit term-color-white)))))
+
