c2e34ff9f2ee7c1dd832f681ce3fa62988aaacdf
Ivan Diaz <ivdiazsa@microsoft.com>
2024-06-06 15:26:02 -0700

    Good progress in remaking my Japanese Study App!

--- japanese-studies/reviewjapanese/quiztools.jl
+++ japanese-studies/reviewjapanese/quiztools.jl
@@ -2,25 +2,26 @@
 
 module QuizTools
 
-export getwords
+export get_wordpool
 export runquiz
 
 """
 """
 struct JPWord
+    english::String
     kana::String
     kanji::String
-    english::String
 end
 
 """
 """
-function getwords(jporgfile::String)
-    words = Vector{JPWord}()
-    println("\nReading the word pool from '$jporgfile'...")
+function addwords_fromfile!(wordpool::Vector{JPWord}, jporgfile::String)
+    println("Reading the word list from '$jporgfile' into the word pool...")
 
     for line in eachline(jporgfile)
-        if ! startswith(line, "-") continue end
+        if ! startswith(line, "-")
+            continue
+        end
 
         parts = split(line, " ")
 
@@ -33,10 +34,10 @@ function getwords(jporgfile::String)
         kanjiwriting = strip(parts[3], ['(', ')', ':'])
         english = join(parts[4:length(parts)], " ")
 
-        push!(words, JPWord(kanawriting, kanjiwriting, english))
+        push!(wordpool, JPWord(english, kanawriting, kanjiwriting))
     end
 
-    return words
+    return wordpool
 end
 
 """
@@ -47,19 +48,18 @@ function ask(question::JPWord, from::String, to::String)
 
     if cmp(from, "kanji") == 0
         from_prompt = "the following Kanji '$(question.kanji)'"
-
         if cmp(to, "english") == 0
-            to_prompt = "meaning"
+            to_prompt = "the meaning"
         elseif cmp(to, "kana") == 0
-            to_prompt = "kana spelling"
+            to_prompt = "the kana spelling"
         end
-
-    else
-        to_prompt = "Kanji"
-        from_prompt = "$(question.kana) when it means $(question.english)"
+    elseif cmp(from, "kana&english") == 0
+        from_prompt = "'$(question.kana)' when it means '$(question.english)'"
+        to_prompt = "the Kanji"
     end
 
-    println("\nWhat is the $to_prompt of $from_prompt?")
+    answer = readline(stdin)
+    return answer
 end
 
 """
@@ -69,49 +69,154 @@ function displayresults(
     expected::Int,
     misses::Vector{@NamedTuple{q::JPWord, a::String}}
 )
-    print("\nYou had $actual/$expected correct answers")
-
-    if actual == expected
-        print("!\n")
-        println("Way to go! You got all questions of this session right!")
-    else
-        print(".\n")
-        println("You missed $(expected - actual) questions :(")
-        println("\nHere are the missed questions, so you can review them:\n")
-
-        for miss in misses
-            println("- Kanji:    $(miss.q.kanji)")
-            println("  Kana:     $(miss.q.kana)")
-            println("  Meanings: $(miss.q.english)\n")
-            println("You answered: '$(miss.a)'\n")
-        end
-    end
 end
 
 """
 """
-function runquiz(wordlist::Vector{JPWord}, numqs::Int, quiztype::String)
+function get_wordpool(jpfiles::Vector{String})
+    pool = Vector{JPWord}()
+    print("\n")
+
+    for file in jpfiles
+        addwords_fromfile!(pool, file)
+    end
+
+    return pool
+end
+
+"""
+"""
+function runquiz(wordpool::Vector{JPWord}, params::Dict{String,Any})
+    from = params["ask"]
+    to = params["answer"]
+    numqs = params["num-questions"]
+    poolsize = length(wordpool)
+
     numcorrects = 0
     misses = Vector{@NamedTuple{q::JPWord, a::String}}()
-    qindices = rand(1:length(wordlist), numqs)
+    qcounter = 1
 
-    qfrom, qto = map(s -> String(s), split(quiztype, '2'))
-
-    for qi in qindices
-        question = wordlist[qi]
-        ask(question, qfrom, qto)
-        answer = readline(stdin)
-
-        expected = lowercase(getproperty(question, Symbol(qto)))
-
-        if ! isempty(answer) && occursin(lowercase(answer), expected)
-            numcorrects += 1
-        else
-            push!(misses, (q=question, a=answer))
-        end
+    while qcounter <= numqs
+        i = (rand(1:poolsize) * rand(1:poolsize) * rand(1:poolsize)) % poolsize
+        question = wordpool[i]
+        answer = ask(question, from, to)
     end
-
-    displayresults(numcorrects, numqs, misses)
 end
 
 end
+
+# module QuizTools
+
+# export getwords
+# export runquiz
+
+# """
+# """
+# struct JPWord
+#     kana::String
+#     kanji::String
+#     english::String
+# end
+
+# """
+# """
+# function getwords(jporgfile::String)
+#     words = Vector{JPWord}()
+#     println("\nReading the word pool from '$jporgfile'...")
+
+#     for line in eachline(jporgfile)
+#         if ! startswith(line, "-") continue end
+
+#         parts = split(line, " ")
+
+#         # Parts[1] = The dash denoting the list item
+#         # Parts[2] = Hiragana/Katakana writing
+#         # Parts[3] = Kanji writing
+#         # Parts[4..] = Definitions
+
+#         kanawriting = parts[2]
+#         kanjiwriting = strip(parts[3], ['(', ')', ':'])
+#         english = join(parts[4:length(parts)], " ")
+
+#         push!(words, JPWord(kanawriting, kanjiwriting, english))
+#     end
+
+#     return words
+# end
+
+# """
+# """
+# function ask(question::JPWord, from::String, to::String)
+#     from_prompt = ""
+#     to_prompt = ""
+
+#     if cmp(from, "kanji") == 0
+#         from_prompt = "the following Kanji '$(question.kanji)'"
+
+#         if cmp(to, "english") == 0
+#             to_prompt = "meaning"
+#         elseif cmp(to, "kana") == 0
+#             to_prompt = "kana spelling"
+#         end
+
+#     else
+#         to_prompt = "Kanji"
+#         from_prompt = "$(question.kana) when it means $(question.english)"
+#     end
+
+#     println("\nWhat is the $to_prompt of $from_prompt?")
+# end
+
+# """
+# """
+# function displayresults(
+#     actual::Int,
+#     expected::Int,
+#     misses::Vector{@NamedTuple{q::JPWord, a::String}}
+# )
+#     print("\nYou had $actual/$expected correct answers")
+
+#     if actual == expected
+#         print("!\n")
+#         println("Way to go! You got all questions of this session right!")
+#     else
+#         print(".\n")
+#         println("You missed $(expected - actual) questions :(")
+#         println("\nHere are the missed questions, so you can review them:\n")
+
+#         for miss in misses
+#             println("- Kanji:    $(miss.q.kanji)")
+#             println("  Kana:     $(miss.q.kana)")
+#             println("  Meanings: $(miss.q.english)\n")
+#             println("You answered: '$(miss.a)'\n")
+#         end
+#     end
+# end
+
+# """
+# """
+# function runquiz(wordlist::Vector{JPWord}, numqs::Int, quiztype::String)
+#     numcorrects = 0
+#     misses = Vector{@NamedTuple{q::JPWord, a::String}}()
+#     qindices = rand(1:length(wordlist), numqs)
+
+#     qfrom, qto = map(s -> String(s), split(quiztype, '2'))
+
+#     for qi in qindices
+#         question = wordlist[qi]
+#         ask(question, qfrom, qto)
+#         answer = readline(stdin)
+
+#         expected = lowercase(getproperty(question, Symbol(qto)))
+
+#         if ! isempty(answer) && occursin(lowercase(answer), expected)
+#             numcorrects += 1
+#         else
+#             push!(misses, (q=question, a=answer))
+#         end
+#     end
+
+#     displayresults(numcorrects, numqs, misses)
+# end
+
+# end
--- japanese-studies/reviewjapanese/reviewjapanese.jl
+++ japanese-studies/reviewjapanese/reviewjapanese.jl
@@ -1,49 +1,13 @@
 #!/usr/bin/julia
 
+using Random
+
+include("commandline_parser.jl")
 include("quiztools.jl")
 
-"""
-"""
-function getparams()
-    wordfile = ""
-    numquestions = 0
+params = CommandLineParser.parsecmdline(ARGS)
 
-    if length(ARGS) < 2
-        print("\n")
+wordpool = QuizTools.get_wordpool(params["word-files"])
+shuffle!(wordpool)
 
-        if length(ARGS) < 1
-            print("Enter the file where the word list is: ")
-            wordfile = readline(stdin)
-        else
-            wordfile = ARGS[1]
-        end
-
-        print("Enter the number of questions you want to review: ")
-        numquestions = parse(Int, readline(stdin))
-
-    else
-        wordfile = ARGS[1]
-        numquestions = parse(Int, ARGS[2])
-
-    end
-
-    return wordfile, numquestions
-end
-
-"""
-"""
-function main()
-    wordfile, numquestions = getparams()
-    wordpool = QuizTools.getwords(wordfile)
-    quiztype = "kanji2english"
-
-    # Options for the Quiz:
-    # - Kanji to English
-    # - Kanji to Kana
-    # - Kana/English to Kanji
-
-    QuizTools.runquiz(wordpool, numquestions, quiztype)
-    return 0
-end
-
-main()
+QuizTools.runquiz(wordpool, params)
