95c389d57f4b553d654ec7db60cf825442bef702
Ivan Diaz <ivdiazsa@microsoft.com>
2024-06-11 14:49:30 -0700

    Main app works, but the evaluation does not function properly. Casing and missing substrings are the most likely cause.

--- japanese-studies/review_japanese.py
+++ japanese-studies/review_japanese.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
-import sys
 import argparse
 import random
+import sys
+
+from collections import namedtuple
 
 # ********** JPWord Class **********
 
@@ -16,6 +18,10 @@ class JPWord:
         return "\n".join([f"English: {self.english}",
                           f"Kana: {self.kana}",
                           f"Kanji: {self.kanji}"])
+
+# ********** Question/Answer Set Type **********
+
+QA = namedtuple('QA', ['question', 'answer'])
 
 # ---------- Parse_CmdLine() ----------
 
@@ -39,7 +45,7 @@ def parse_cmdline(args: list[str]) -> argparse.Namespace:
 # ---------- Get_Words_From_File() ----------
 
 def get_words_from_file(pool: list[JPWord], filename: str) -> None:
-    with open(filename) as fp:
+    with open(filename, encoding='utf8') as fp:
         for line in fp:
             if not line.startswith("- "): continue
 
@@ -48,12 +54,12 @@ def get_words_from_file(pool: list[JPWord], filename: str) -> None:
             # Parts[2] = Kanji Writing
             # Parts[3..] = Meanings in English
 
-            parts = line.strip().split(" ")
+            parts = line.strip().split()
             kanawriting = parts[1]
             kanjiwriting = ''.join([c for c in parts[2] if not c in '():'])
             english = ' '.join(parts[3:])
 
-            word = JPWord(english, kanawriting, kanjiwriting)# ---------- Run_Quiz() ----------
+            word = JPWord(english, kanawriting, kanjiwriting)
             pool.append(word)
 
 # ---------- Generate_Word_Pool() ----------
@@ -66,13 +72,78 @@ def generate_word_pool(files: list[str]) -> list[JPWord]:
 
     return word_pool
 
+# ---------- Ask() ----------
+
+def ask(question: JPWord, qkind: str) -> str:
+    from_prompt = ''
+    to_prompt = ''
+
+    if qkind == 'read':
+        from_prompt = 'of the following '
+        to_prompt = 'the meaning'
+        if not question.kanji:
+            from_prompt += f"word '{question.kana}'"
+        else:
+            from_prompt += f"kanji '{question.kanji}'"
+
+    elif qkind == 'write':
+        if not question.kanji:
+            from_prompt = f"for '{question.english}'"
+            to_prompt = 'the japanese word'
+        else:
+            from_prompt = f"of '{question.kana}' when it means '{question.english}'"
+            to_prompt = 'the kanji'
+
+    answer = input(f"\nWhat is {to_prompt} {from_prompt}?\n")
+    return answer.lower()
+
 # ---------- Run_Quiz() ----------
 
 def run_quiz(word_pool: list[JPWord], kind: str, num_questions: int) -> None:
     q_counter = 1
+    corrects = []
+    misses = []
 
     while q_counter <= num_questions:
-        # Run Quiz!
+        q_counter += 1
+        question = random.choice(word_pool)
+
+        if kind == 'read':
+            expected_answer_type = 'english'
+        elif kind == 'write':
+            expected_answer_type = 'kanji' if question.kanji else 'kana'
+
+        answer = ask(question, kind)
+        expected = getattr(question, expected_answer_type).lower()
+
+        if answer == expected:
+            corrects.append(QA(question, answer))
+        else:
+            misses.append(QA(question, answer))
+
+    display_results(corrects, misses, num_questions)
+
+# ---------- Display_Results() ----------
+
+def display_results(corrects: list[QA], misses: list[QA], num_questions: int) -> None:
+    num_corrects = len(corrects)
+    num_misses = len(misses)
+
+    print(f"\nYou had {num_corrects}/{num_questions} correct answers",
+          end="!\n" if num_corrects == num_questions else ".\n")
+
+    if num_corrects == num_questions:
+        print("\nWay to go! You got them all right this time!\n")
+    else:
+        print("\nHere are the ones you missed, so you can review them :)")
+        for miss in misses:
+            print(f"\n{str(miss.question)}")
+            print(f"You answered: '{miss.answer}'")
+
+    print("\nHere are the ones you got right:")
+    for correct in corrects:
+        print(f"\n{str(correct.question)}")
+        print(f"You answered: '{correct.answer}'")
 
 # ---------- Setup_And_Run_App() ----------
 
