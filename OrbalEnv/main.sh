a8dad8232fd31d6e0010632a94df3a39d2c33fe1
Ivan Diaz <ivdiazsa@microsoft.com>
2024-08-21 16:05:08 -0700

    Got the main bash script working!

--- OrbalEnv/main.sh
+++ OrbalEnv/main.sh
@@ -3,6 +3,10 @@
 # GENERAL TODO: Add safeguards where needed.
 
 MAIN_SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-# results=$(find . -mindepth 2 -maxdepth 2 -type f -print)
+PLUGIN_ENTRIES=$(find $MAIN_SCRIPT_DIR -mindepth 2 -maxdepth 2 -name '*.sh' -type f -print)
 
-echo 'Orbal Environment Main Script Under Construction!'
+for script in "${PLUGIN_ENTRIES[@]}"
+do
+    echo -e "\nSourcing $script...\n"
+    source $script
+done
