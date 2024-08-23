d2ee6bbe99a5d4bfb0887c169537eb6fc5d0cb40
Ivan Diaz <ivdiazsa@microsoft.com>
2024-08-23 10:23:57 -0700

    Fixed the loop to source all plugins in a Bash environment.

--- OrbalEnv/main.sh
+++ OrbalEnv/main.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-# GENERAL TODO: Add safeguards where needed.
-
 MAIN_SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PLUGIN_ENTRIES=$(find $MAIN_SCRIPT_DIR -mindepth 2 -maxdepth 2 -name '*.sh' -type f -print)
+PLUGINS_CMD=(find $MAIN_SCRIPT_DIR -mindepth 2 -maxdepth 2 -name '*.sh' -type f -print)
 
-for script in "${PLUGIN_ENTRIES[@]}"
+while IFS= read -r script
 do
     echo -e "\nSourcing $script...\n"
     source $script
-done
+done < <("${PLUGINS_CMD[@]}")
